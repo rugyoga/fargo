a5a7929e04fecf958e0175361e40d1b6cbc95cae
Guy Argo <Guy.argo@gmail.com>
2024-09-19 00:37:05 -0700

    First crack at race to n logic

--- lib/fargo/math.ex
+++ lib/fargo/math.ex
@@ -21,4 +21,32 @@ defmodule Fargo.Math do
   def m_choose_n(m, n_lo, n_hi) do
     Enum.map(m_choose_n(m-1, n_lo+1, n_hi), &[n_lo | &1]) ++ m_choose_n(m, n_lo+1, n_hi)
   end
+
+  def race_to_n_outcomes(n) do
+    fn {a, b} -> a == n or b == n end
+    |> race()
+    |> Enum.group_by(& &1, fn _ -> 1 end)
+    |> Enum.map(fn {k, v} -> {k, Enum.sum(v)} end)
+  end
+
+  def race_to_n(n) do
+    counts = n |> race_to_n_outcomes() |> Enum.filter(fn {{w, _}, _} -> w == n end)
+
+    fn p ->
+      counts
+      |> Enum.map(fn {{w, l}, count} -> :math.pow(p, w) * :math.pow(1-p, l) * count end)
+      |> Enum.sum
+    end
+  end
+
+  def race(done?), do: race(done?, [], [{0, 0}])
+
+  def race(_, completed, []), do: completed
+  def race(done?, completed, [{wins, losses} = candidate | candidates]) do
+    if done?.(candidate) do
+      race(done?, [candidate | completed], candidates)
+    else
+      race(done?, completed, [{wins+1, losses} | [{wins, losses+1} | candidates]])
+    end
+  end
 end
