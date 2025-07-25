af37330623ddb652858da8ac4702c92927d9b8a5
Guy Argo <Guy.argo@gmail.com>
2025-07-25 16:51:52 -0700

    add balck willows and bus stop pick em up back in

--- lib/fargo/scraper.ex
+++ lib/fargo/scraper.ex
@@ -111,7 +111,8 @@ defmodule Fargo.Scraper do
         |> Enum.sort_by(fn {_, {max_strength, _}} -> max_strength end) } end)
   end
 
-  # def cleanse("The Black Willows"), do: "Black Willows"
+  def cleanse("The Black Willows"), do: "Black Willows"
+  def cleanse("Bus Stop Pick Me up"), do: "Bus Stop Pick Me Up"
   # def cleanse("Il Pirata Not Your FN Cisters"), do: "Il Pirata Not Your F'ing Cisters"
   #def cleanse("Happy HALLers"), do: "Happy Diamond CueTT's"
   # def cleanse("Gino’s Diamonds"), do: "Gino's Diamonds"
